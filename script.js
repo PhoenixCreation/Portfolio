0a493617dfdf810773f9bfe669efc162103e6f28
phoenix <phs28052001@gmail.com>
2021-01-12 13:33:19 +0530

    Starting base HTML and CSS.

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // DOM elements
 var pagers = document.getElementsByClassName("pager--indicator");
+var myFullpage = new fullpage("#fullpage", {
+  anchors: ["home", "about", "projects", "contact"],
+  menu: "#myMenu",
+  verticalCentered: false,
+});
