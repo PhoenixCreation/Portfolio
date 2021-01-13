ee86de7638dd67aa4854f0d5fe9729e9a4e06a3a
phoenix <phs28052001@gmail.com>
2021-01-13 20:25:03 +0530

    Some important changes and twicks.

--- script.js
+++ script.js
@@ -49,3 +49,17 @@ document.addEventListener("mousemove", (event) => {
   meteor.style.left = x + "px";
   meteor.style.top = y + "px";
 });
+
+// Mobile check if required later....
+// let mobile = false
+// if (
+//   navigator.userAgent.match(/Android/i) ||
+//   navigator.userAgent.match(/webOS/i) ||
+//   navigator.userAgent.match(/iPhone/i) ||
+//   navigator.userAgent.match(/iPad/i) ||
+//   navigator.userAgent.match(/iPod/i) ||
+//   navigator.userAgent.match(/BlackBerry/i) ||
+//   navigator.userAgent.match(/Windows Phone/i)
+// ) {
+//   mobile = true
+// }
