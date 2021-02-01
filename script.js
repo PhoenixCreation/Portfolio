5ddf67d2b0103b705b1f73f5b717656dd26ce284
PhoenixCreation <phs28052001@gmail.com>
2021-02-01 19:32:31 +0530

    checking netlify mailing.

--- script.js
+++ script.js
@@ -134,18 +134,3 @@ if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     }
   );
 }
-
-// document.querySelector("form").addEventListener("submit", handleSubmit);
-
-// const handleSubmit = (e) => {
-//   e.preventDefault();
-//   let myForm = document.getElementById("pizzaOrder");
-//   let formData = new FormData(myForm);
-//   fetch("/", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: new URLSearchParams(formData).toString(),
-//   })
-//     .then(() => console.log("Form successfully submitted"))
-//     .catch((error) => alert(error));
-// };
